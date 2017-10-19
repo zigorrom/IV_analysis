88f6d83a0fc876ba27e0a9d696beb8800806b90a
zigorrom <zigorrom@gmail.com>
2017-10-19 17:20:23 +0200

    Threshold Voltage

--- IV_analysis/analysis.py
+++ IV_analysis/analysis.py
@@ -3,6 +3,9 @@ import ntpath
 import re
 import pandas as pd
 
+from scipy.interpolate import interp1d
+from scipy import optimize
+
 def parse_measurement_filename(filename):
     filename = ntpath.basename(filename)
     pattern = re.compile("^[tT](?P<number>[0-9]*)[-_]IV(?P<type>(lg|bg|ds))[-_](?P<info>.*?)\.dat$") #(?P<transistor>[0-9]?)[-_](?P<wafer>.*?)[-_](?P<chip>.*?)[-_]
@@ -53,3 +56,33 @@ for index,row in linear_transfer_curves.iterrows():
     length = float(transistor["Length"])
     print(number)
     print("\t W = {0}; L = {1};".format(width,length))
+
+    value_current = initial_current * width / length
+
+    data_file_path = os.path.join(working_folder, fname)
+
+    transfer_data = pd.DataFrame.from_csv(data_file_path, index_col = None)
+    #Gate voltage, Gate current, Gate timestamp, Drain voltage, Drain current, Drain timestamp
+    gate_voltage, gate_current, gate_timestamp, drain_voltage, drain_current, drain_timestamp = list(transfer_data)
+    
+    
+
+    voltages = transfer_data[gate_voltage]
+    
+    currents = transfer_data[drain_current]
+    max_current = currents.max()
+    currents = (currents-max_current).abs()
+
+
+    print(value_current)
+
+    inverse_transfer_curve = interp1d(currents, voltages)
+
+    treshold_voltage = inverse_transfer_curve(value_current)
+    
+    print(treshold_voltage)
+
+    overdrive_gate_voltage = voltages - treshold_voltage
+
+
+    
